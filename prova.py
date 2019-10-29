c6e450dd05995b366d5e323b7c8dd9b702e8efb0
lai2 <informatica@lai2>
2019-10-29 14:26:10 +0100

    aggiunto file in python

---
+++ prova.py
@@ -0,0 +1,12 @@
+
+print('Somma di due numeri');
+print('Inserisci il primo numero');
+a = input();
+
+print('Inserisci il secondo numero');
+b = input();
+
+def Somma():
+	return a + b;
+
+print('Il risultato e\' ' + str(Somma()));
